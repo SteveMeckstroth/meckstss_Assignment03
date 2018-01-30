916fa81c57457bd1d178aaac3f4cffbe97489f6a
Stephen Meckstroth <meckstss@gmail.com>
2018-01-30 18:02:19 -0500

    Commit with more prime number logic

--- PrimeNumbers/src/primeNumbers/Main.java
+++ PrimeNumbers/src/primeNumbers/Main.java
@@ -1,3 +1,12 @@
+/**
+ * @author Stephen Meckstroth
+ * Assignment 03
+ * Course IT2045 Section 001
+ * Due DAte: Jan 30, 2018
+ * Description: Create an ecipse java project that determines if a number is prime or not
+ * Citations: N/A
+ */
+
 package primeNumbers;
 
 import java.math.BigInteger;
@@ -5,16 +14,19 @@ import java.math.BigInteger;
 public class Main {
 
 	public static void main(String[] args) {
-		//TODO: test cases should be bigger than :
-		System.out.println(Long.MAX_VALUE);
+		//Instantiate the Main class
+		Main m = new Main();
 		
-		BigInteger bi = new BigInteger("123456789012345678901234567890");
-		// Print it
+		//BigInteger bi = new bigInteger("110384710234871028347103741027412390471");
+		BigInteger bi = new BigInteger("7");
+		
+		//Print the result of the prime test
 		System.out.println(bi.toString());
-		// How do you add to a BigInteger?
-		bi = bi.add(new BigInteger("1"));
-		System.out.println(bi.toString());
-
+		if (m.isPrime(bi) == false) {
+			System.out.println(bi.toString() + " is NOT Prime!");
+		} else {
+			System.out.println(bi.toString() + " is Prime!");
+		}
 	}
 	/**
 	 * Check to see if the number stored in a BigInteger object is prime
@@ -22,9 +34,17 @@ public class Main {
 	 * @return True if number is prime, false otherwise
 	 */
 	public Boolean isPrime(BigInteger number){
-		Boolean result = true;
-		// Your code
+		Boolean result = false;
+		BigInteger compareTo = new BigInteger("1");
 		
+		//Zero is assumed to be even, because number will be validated to be > 2
+		//number must end in an odd number
+		int[] oddNumbers = {1,3,5,7,9};
+		for(int i=0; i< oddNumbers.length-1; i++){
+			if(Integer.valueOf(number.toString().substring(number.toString().length()-1)) == oddNumbers[i]){
+				
+			}
+		}
 		return result;
 	}
 
