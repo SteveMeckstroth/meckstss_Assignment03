5c853bbce40950320333c191c24c62b673fc53bc
Steve Meckstroth <meckstss@gmail.com>
2018-02-04 19:12:56 -0500

    test github commit

--- PrimeNumbers/src/primeNumbers/Main.java
+++ PrimeNumbers/src/primeNumbers/Main.java
@@ -42,7 +42,7 @@ public class Main {
 		int[] oddNumbers = {1,3,5,7,9};
 		for(int i=0; i< oddNumbers.length-1; i++){
 			if(Integer.valueOf(number.toString().substring(number.toString().length()-1)) == oddNumbers[i]){
-				
+				//test github commit is working
 			}
 		}
 		return result;
